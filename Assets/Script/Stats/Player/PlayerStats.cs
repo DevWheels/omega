84b2547243245500b1dfeb1ac8f4713579aef63d
Lukhern <90791937+Luriko@users.noreply.github.com>
2025-04-01 13:12:28 +0200

    exported some methods from PlayerStats.cs that works with UI to PlayerUI.cs

--- Assets/Script/Stats/Player/PlayerStats.cs
+++ Assets/Script/Stats/Player/PlayerStats.cs
@@ -8,90 +8,68 @@ using UnityEngine.UI;
 using UnityEngine.Tilemaps;
 using System.IO;
 using Mirror;
+using Script.Stats.Player;
+
 public class PlayerStats : NetworkBehaviour
 {
-    // [Header("Player Stats texts")]
-    // public TMP_Text hp_text;
-    // public TMP_Text hp_text_inv;
-    //
-    // public TMP_Text mana_text;
-    // public TMP_Text mana_text_inv;
-    //
-    // public TMP_Text armor_text;
-    // public TMP_Text strength_text;
-    // public TMP_Text sanity_text;
-    // public TMP_Text agility_text;
-    // public TMP_Text luck_text;
-    // public TMP_Text speed_text;
-    // public TMP_Text level_text;
-    // public TMP_Text level_text_percent_for_new_level;
-    // public TMP_Text ability_points_text;
-    // public TMP_Text exp_text;
-    //
-    // [Header("Player Stats UI buttons")]
-    // public GameObject strength_up;
-    // public GameObject sanity_up;
-    // public GameObject agility_up;
-    // public GameObject luck_up;
-    // public GameObject speed_up;
 
     [SyncVar]
-    private int max_hp = 300;
+    protected int max_hp = 300;
     [SyncVar]
-    private int currently_hp = 300;
+    protected int currently_hp = 300;
     [SyncVar]
-    private int max_mana = 65;
+    protected int max_mana = 65;
     [SyncVar]
-    private int currently_mana = 65;
+    protected int currently_mana = 65;
 
     [SyncVar]
-    private int armor = 35;
+    protected int armor = 35;
     [SyncVar]
-    private int xp_needed = 1000;
+    protected int xp_needed = 1000;
     [SyncVar]
-    private int xp_needed_per_lvl = 1000;
+    protected int xp_needed_per_lvl = 1000;
     [SyncVar]
-    private int xp_currently = 0;
+    protected int xp_currently = 0;
     [SyncVar]
-    private int lvl = 1;
+    protected int lvl = 1;
     [SyncVar]
-    private int ability_points;
+    protected int ability_points;
 
     [SyncVar]
-    private int strength = 1;
+    protected int strength = 1;
     [SyncVar]
-    private int sanity = 1;
+    protected int sanity = 1;
     [SyncVar]
-    private int agility = 1;
+    protected int agility = 1;
     [SyncVar]
-    private int luck = 1;
+    protected int luck = 1;
     [SyncVar]
-    private int speed = 1;
+    protected int speed = 1;
 
     [SyncVar]
-    private float timer = 0f;
+    protected float timer = 0f;
     [SyncVar]
-    private float interval = 1f;
+    protected float interval = 1f;
 
     [SyncVar]
-    private float hp_regeneration;
+    protected float hp_regeneration;
     [SyncVar]
-    private float mana_regeneration;
+    protected float mana_regeneration;
 
-    // public Image healthBarFill;  //Добавил Денис для отображения hp на canvas
-    // public Image manahBarFill;  //Добавил Денис для отображения mana на canvas
-    // public Image expBarFill;  //Добавил Денис для отображения hp на персанаже на canvas
 
-    private PlayerMovement playerMovement;
-    private Find find;
+
+    protected PlayerMovement playerMovement;
+    private PlayerUI playerUI;
+
+    
     private void OnApplicationQuit()
     {
         //SavePlayerData(); // Сохраняем данные при выходе
     }
     void Start()
     {
-        find = new();
-        FindAllUI();
+
+        FindPlayerComponents();
         //StartCoroutine(FindObjectsAsync());
         //Debug.Log(Application.persistentDataPath);
 
@@ -107,7 +85,7 @@ public class PlayerStats : NetworkBehaviour
         }
         AddExperience(2); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ДЛЯ ТЕСТА удалить потом
         CheckHpAndMana();
-        UpdateUI();
+        playerUI.UpdateUI();
 
         // Регенерация здоровья и маны раз в секунду, если не максимальное здоровье или мана
         // может быть переделать это в карутину в будущем
@@ -118,8 +96,6 @@ public class PlayerStats : NetworkBehaviour
             timer = 0f; // Сброс времени на 1 секунду
         }
     }
-
-
 
     public void UseItem(Item item)
     {
@@ -141,40 +117,9 @@ public class PlayerStats : NetworkBehaviour
             }
         }
 
-        UpdateUI(); // Обновляем интерфейс после использования предмета
+        playerUI.UpdateUI(); // Обновляем интерфейс после использования предмета
     }
-
-
-
-
-    private void UpdateHealthBar() //Добавил Денис для отображения hp на canvas
-    {
-
-        if (healthBarFill != null)
-        {
-            float healthPercentage = (float)currently_hp / max_hp;
-            healthBarFill.fillAmount = healthPercentage;
-        }
-    }
-
-    private void UpdateEXPBar() //Добавил Юра для отображения exp на canvas
-    {
-        if (expBarFill != null)
-        {
-            float expPercentage = (float)xp_currently / xp_needed;
-            expBarFill.fillAmount = expPercentage;
-            level_text_percent_for_new_level.text = expPercentage.ToString("0.00%");
-        }
-    }
-
-    private void UpdateManaBar() //Добавил Денис для отображения mana на canvas
-    {
-        if (manahBarFill != null)
-        {
-            float manaPercentage = (float)currently_mana / max_mana;
-            manahBarFill.fillAmount = manaPercentage;
-        }
-    }
+    
 
     private void LevelUp()
     {
@@ -189,7 +134,7 @@ public class PlayerStats : NetworkBehaviour
 
             xp_currently = 0;
             ability_points += 1;
-            SetStateOfAbilityUpdateButtons();
+            playerUI.SetStateOfAbilityUpdateButtons();
         }
 
         //если уровень выше 100, то просто прибавляем к нему 1, ничего не выдавая
@@ -208,14 +153,14 @@ public class PlayerStats : NetworkBehaviour
     /// <summary>
     /// Обновляет характеристика игрока, выдавая им новые значения
     /// </summary>
-    private void UpdateAllStats()
+    protected void UpdateAllStats()
     {
         xp_needed = (int)(xp_needed_per_lvl + xp_needed_per_lvl * (lvl - 1));
         int strength_hp = (strength - 1) * 20;
         max_hp = (int)300 + strength_hp;
         int sanity_mana = (sanity - 1) * 10;
         max_mana = (int)65 + sanity_mana;
-        UpdateUI();
+        playerUI.UpdateUI();
     }
 
     public void AddExperience(int experience)
@@ -225,7 +170,7 @@ public class PlayerStats : NetworkBehaviour
         {
             LevelUp();
         }
-        UpdateUI();
+        playerUI.UpdateUI();
     }
     [Client]
     private void Regeneration()
@@ -237,14 +182,14 @@ public class PlayerStats : NetworkBehaviour
         {
             currently_hp += (int)hp_regeneration;
             hp_regeneration = 0f; // Сбросить значения регенерации
-            UpdateUI();
+            playerUI.UpdateUI();
 
         }
         if (mana_regeneration >= 1 && currently_mana > max_mana * 0.1f)
         {
             currently_mana += (int)mana_regeneration;
             mana_regeneration = 0f; // Сбросить значения регенерации
-            UpdateUI();
+            playerUI.UpdateUI();
         }
     }
     public void TakeHit(int damage)
@@ -252,99 +197,7 @@ public class PlayerStats : NetworkBehaviour
         currently_hp -= damage;
         UpdateEverything();
     }
-    public void IncreaseStrength()
-    {
-        if (ability_points > 0)
-        {
-            strength++;
-            ability_points -= 1;
-            UpdateAllStats();
-            SetStateOfAbilityUpdateButtons();
-        }
-    }
-    public void IncreaseSanity()
-    {
-        if (ability_points > 0)
-        {
-            sanity++;
-            ability_points -= 1;
-            UpdateAllStats();
-            SetStateOfAbilityUpdateButtons();
 
-
-        }
-
-    }
-    public void IncreaseAgility()
-    {
-        if (ability_points > 0)
-        {
-            agility++;
-            ability_points -= 1;
-            UpdateAllStats();
-
-            SetStateOfAbilityUpdateButtons();
-
-        }
-    }
-    public void IncreaseLuck()
-    {
-        if (ability_points > 0)
-        {
-            luck++;
-            ability_points -= 1;
-            UpdateAllStats();
-            SetStateOfAbilityUpdateButtons();
-
-        }
-    }
-    public void IncreaseSpeed()
-    {
-        if (ability_points > 0)
-        {
-            speed++;
-            ability_points -= 1;
-            float speed_multiply = 0.05f;
-            playerMovement.moveSpeed += speed_multiply;
-            UpdateAllStats();
-            SetStateOfAbilityUpdateButtons();
-
-        }
-    }
-    /// <summary>
-    /// Обновляет UI для игрока
-    /// </summary>
-    [Client]
-    public void UpdateUI()
-    {
-        //Обновляет визуально картинку для игрока в процентном соотношении
-        UpdateHealthBar();
-        UpdateManaBar();
-        UpdateEXPBar();
-
-        //Обновляет текст
-        hp_text.text = $"{currently_hp}/{max_hp}";
-        hp_text_inv.text = $"{currently_hp}/{max_hp}";
-
-        mana_text.text = $"{currently_mana} / {max_mana}";
-        mana_text_inv.text = $"{currently_mana} / {max_mana}";
-
-        armor_text.text = $"{armor}";
-
-        level_text.text = $"{lvl}";
-        exp_text.text = $"Опыт: {xp_currently}/{xp_needed}";
-
-        strength_text.text = $"{strength}";
-        sanity_text.text = $"{sanity}";
-        agility_text.text = $"{agility}";
-        luck_text.text = $"{luck}";
-        speed_text.text = $"{speed}";
-        if (ability_points > 0)
-            ability_points_text.text = $"Очки характеристиков: {ability_points}";
-        else
-            ability_points_text.text = "";
-
-    }
     [Client]
     private void CheckHpAndMana()
     {
@@ -451,83 +304,18 @@ public class PlayerStats : NetworkBehaviour
     [Client]
     private void UpdateEverything()
     {
-        UpdateUI();
+        playerUI.UpdateUI();
         UpdateAllStats();
         //LoadPlayerData();
-
     }
     /// <summary>
     /// Корутина для поиска всех UI элементов
     /// </summary>
     [Client]
-    private void FindAllUI()
+    private void FindPlayerComponents()
     {
-        hp_text = find.FindUIElement<TMP_Text>("hp text");
-        hp_text_inv = find.FindUIElement<TMP_Text>("hp text inv");
-
-        mana_text = find.FindUIElement<TMP_Text>("mana text");
-        mana_text_inv = find.FindUIElement<TMP_Text>("mana text inv");
-
-        armor_text = find.FindUIElement<TMP_Text>("armor text");
-
-        strength_text = find.FindUIElement<TMP_Text>("strength text");
-        sanity_text = find.FindUIElement<TMP_Text>("sanity text");
-        agility_text = find.FindUIElement<TMP_Text>("agility text");
-        luck_text = find.FindUIElement<TMP_Text>("luck text");
-        speed_text = find.FindUIElement<TMP_Text>("speed text");
-        level_text = find.FindUIElement<TMP_Text>("lvl full");
-        level_text_percent_for_new_level = find.FindUIElement<TMP_Text>("lvl procent");
-
-        ability_points_text = find.FindUIElement<TMP_Text>("ability points text");
-        exp_text = find.FindUIElement<TMP_Text>("exp text");
-        healthBarFill = find.FindUIElement<Image>("Healthbar");
-        manahBarFill = find.FindUIElement<Image>("Manabar");
-        expBarFill = find.FindUIElement<Image>("xp bar filled");
-
-
         playerMovement = gameObject.GetComponent<PlayerMovement>();
-
-        strength_up = find.FindGameObject("StrengthUp");
-        strength_up.GetComponent<Button>().onClick.AddListener(IncreaseStrength);
-        sanity_up = find.FindGameObject("SanityUp");
-        sanity_up.GetComponent<Button>().onClick.AddListener(IncreaseSanity);
-        agility_up = find.FindGameObject("AgilityUp");
-        agility_up.GetComponent<Button>().onClick.AddListener(IncreaseAgility);
-        luck_up = find.FindGameObject("LuckUp");
-        luck_up.GetComponent<Button>().onClick.AddListener(IncreaseLuck);
-        speed_up = find.FindGameObject("SpeedUp");
-        speed_up.GetComponent<Button>().onClick.AddListener(IncreaseSpeed);
-
-        UpdateUI();
+        playerUI = gameObject.GetComponent<PlayerUI>();
     }
-    /// <summary>
-    /// Выставляет состяние кнопок как активное/неактивное если есть/нет очки(-ов) улучшения
-    /// </summary>
-    private void SetStateOfAbilityUpdateButtons()
-    {
-        if (ability_points > 0)
-        {
-            strength_up.transform.localScale = new Vector3(0.3f, 1, 1); // Устанавливаем нормальный размер, кнопка активна
-            sanity_up.transform.localScale = new Vector3(0.3f, 1, 1);
-            agility_up.transform.localScale = new Vector3(0.3f, 1, 1);
-            luck_up.transform.localScale = new Vector3(0.3f, 1, 1);
-            speed_up.transform.localScale = new Vector3(0.3f, 1, 1);
-            UpdateUI();
-        }
-        else
-        {
-            strength_up.transform.localScale = Vector3.zero; // Устанавливаем размер в ноль, кнопка неактивна
-            sanity_up.transform.localScale = Vector3.zero;
-            agility_up.transform.localScale = Vector3.zero;
-            luck_up.transform.localScale = Vector3.zero;
-            speed_up.transform.localScale = Vector3.zero;
-            ability_points_text.text = "";
-        }
-    }
+
 }
-
-
-
-
-
-
--- Assets/Script/Stats/Player/PlayerUI.cs
+++ Assets/Script/Stats/Player/PlayerUI.cs
@@ -11,7 +11,7 @@ using Mirror;
 
 namespace Script.Stats.Player
 {
-    public class PlayerUI
+    public class PlayerUI : PlayerStats
     {
         [Header("Player Stats texts")]
         public TMP_Text hp_text;
@@ -43,11 +43,192 @@ namespace Script.Stats.Player
         public Image expBarFill;  //Добавил Денис для отображения hp на персанаже на canvas
 
         private PlayerStats playerStats;
-        private Find Find;
-        
-        
-        
+        private Find find => new();
 
+        void Start()
+        {
+            playerStats = gameObject.GetComponent<PlayerStats>();
+        }
+        
+        
+        public void UpdateUI()
+        {
+            //Обновляет визуально картинку для игрока в процентном соотношении
+            UpdateHealthBar();
+            UpdateManaBar();
+            UpdateEXPBar();
+
+            //Обновляет текст
+            hp_text.text = $"{currently_hp}/{max_hp}";
+            hp_text_inv.text = $"{currently_hp}/{max_hp}";
+
+            mana_text.text = $"{currently_mana} / {max_mana}";
+            mana_text_inv.text = $"{currently_mana} / {max_mana}";
+
+            armor_text.text = $"{armor}";
+
+            level_text.text = $"{lvl}";
+            exp_text.text = $"Опыт: {xp_currently}/{xp_needed}";
+
+            strength_text.text = $"{strength}";
+            sanity_text.text = $"{sanity}";
+            agility_text.text = $"{agility}";
+            luck_text.text = $"{luck}";
+            speed_text.text = $"{speed}";
+            if (ability_points > 0)
+                ability_points_text.text = $"Очки характеристиков: {ability_points}";
+            else
+                ability_points_text.text = "";
+
+        }
+        private void UpdateHealthBar() //Добавил Денис для отображения hp на canvas
+        {
+
+            if (healthBarFill != null)
+            {
+                float healthPercentage = (float)currently_hp / max_hp;
+                healthBarFill.fillAmount = healthPercentage;
+            }
+        }
+
+        private void UpdateEXPBar() //Добавил Юра для отображения exp на canvas
+        {
+            if (expBarFill != null)
+            {
+                float expPercentage = (float)xp_currently / xp_needed;
+                expBarFill.fillAmount = expPercentage;
+                level_text_percent_for_new_level.text = expPercentage.ToString("0.00%");
+            }
+        }
+
+        private void UpdateManaBar() //Добавил Денис для отображения mana на canvas
+        {
+            if (manahBarFill != null)
+            {
+                float manaPercentage = (float)currently_mana / max_mana;
+                manahBarFill.fillAmount = manaPercentage;
+            }
+        }
+        public void IncreaseStrength()
+        {
+            if (ability_points > 0)
+            {
+                strength++;
+                ability_points -= 1;
+                UpdateAllStats();
+                SetStateOfAbilityUpdateButtons();
+            }
+        }
+        public void IncreaseSanity()
+        {
+            if (ability_points > 0)
+            {
+                sanity++;
+                ability_points -= 1;
+                UpdateAllStats();
+                SetStateOfAbilityUpdateButtons();
+
+
+            }
+
+        }
+        public void IncreaseAgility()
+        {
+            if (ability_points > 0)
+            {
+                agility++;
+                ability_points -= 1;
+                UpdateAllStats();
+
+                SetStateOfAbilityUpdateButtons();
+
+            }
+        }
+        public void IncreaseLuck()
+        {
+            if (ability_points > 0)
+            {
+                luck++;
+                ability_points -= 1;
+                UpdateAllStats();
+                SetStateOfAbilityUpdateButtons();
+
+            }
+        }
+        public void IncreaseSpeed()
+        {
+            if (ability_points > 0)
+            {
+                speed++;
+                ability_points -= 1;
+                float speed_multiply = 0.05f;
+                playerMovement.moveSpeed += speed_multiply;
+                UpdateAllStats();
+                SetStateOfAbilityUpdateButtons();
+
+            }
+        }
+        
+        public void SetStateOfAbilityUpdateButtons()
+        {
+            if (ability_points > 0)
+            {
+                strength_up.transform.localScale = new Vector3(0.3f, 1, 1); // Устанавливаем нормальный размер, кнопка активна
+                sanity_up.transform.localScale = new Vector3(0.3f, 1, 1);
+                agility_up.transform.localScale = new Vector3(0.3f, 1, 1);
+                luck_up.transform.localScale = new Vector3(0.3f, 1, 1);
+                speed_up.transform.localScale = new Vector3(0.3f, 1, 1);
+                UpdateUI();
+            }
+            else
+            {
+                strength_up.transform.localScale = Vector3.zero; // Устанавливаем размер в ноль, кнопка неактивна
+                sanity_up.transform.localScale = Vector3.zero;
+                agility_up.transform.localScale = Vector3.zero;
+                luck_up.transform.localScale = Vector3.zero;
+                speed_up.transform.localScale = Vector3.zero;
+                ability_points_text.text = "";
+            }
+        }
+        private void FindAllUI()
+        {
+            
+            hp_text = find.FindUIElement<TMP_Text>("hp text");
+            hp_text_inv = find.FindUIElement<TMP_Text>("hp text inv");
+
+            mana_text = find.FindUIElement<TMP_Text>("mana text");
+            mana_text_inv = find.FindUIElement<TMP_Text>("mana text inv");
+
+            armor_text = find.FindUIElement<TMP_Text>("armor text");
+
+            strength_text = find.FindUIElement<TMP_Text>("strength text");
+            sanity_text = find.FindUIElement<TMP_Text>("sanity text");
+            agility_text = find.FindUIElement<TMP_Text>("agility text");
+            luck_text = find.FindUIElement<TMP_Text>("luck text");
+            speed_text = find.FindUIElement<TMP_Text>("speed text");
+            level_text = find.FindUIElement<TMP_Text>("lvl full");
+            level_text_percent_for_new_level = find.FindUIElement<TMP_Text>("lvl procent");
+
+            ability_points_text = find.FindUIElement<TMP_Text>("ability points text");
+            exp_text = find.FindUIElement<TMP_Text>("exp text");
+            healthBarFill = find.FindUIElement<Image>("Healthbar");
+            manahBarFill = find.FindUIElement<Image>("Manabar");
+            expBarFill = find.FindUIElement<Image>("xp bar filled");
+
+            strength_up = find.FindGameObject("StrengthUp");
+            strength_up.GetComponent<Button>().onClick.AddListener(IncreaseStrength);
+            sanity_up = find.FindGameObject("SanityUp");
+            sanity_up.GetComponent<Button>().onClick.AddListener(IncreaseSanity);
+            agility_up = find.FindGameObject("AgilityUp");
+            agility_up.GetComponent<Button>().onClick.AddListener(IncreaseAgility);
+            luck_up = find.FindGameObject("LuckUp");
+            luck_up.GetComponent<Button>().onClick.AddListener(IncreaseLuck);
+            speed_up = find.FindGameObject("SpeedUp");
+            speed_up.GetComponent<Button>().onClick.AddListener(IncreaseSpeed);
+
+            UpdateUI();
+        }
 
     }
+    
 }
