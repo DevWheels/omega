fee7aa8f7a351addf4f2c9f0e77de335fe56db6d
TailsAND <korobok-0312@mail.ru>
2025-06-02 16:26:27 +0200

    fix panel

--- Assets/Script/PanelManager.cs
+++ Assets/Script/PanelManager.cs
@@ -1,47 +1,90 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PanelManager : MonoBehaviour {
+public class PanelManager : MonoBehaviour 
+{
     public GameObject[] panels;
     public Button[] buttons;
     private bool isOpened = false;
-    private int lastOpened;
+    private int lastOpened = 0;
 
-    void Start() {
-        for (int i = 0; i < buttons.Length; i++) {
-            int index = i;
-            buttons[i].onClick.AddListener(() => ShowPanel(index));
+    // Делаем менеджер одиночкой (Singleton)
+    public static PanelManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        // Убеждаемся, что существует только один экземпляр
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
         }
+        else
+        {
+            Instance = this;
+        }
+    }
 
-        // Инициализация: все панели скрыты
-        foreach (var panel in panels) {
+    void Start() 
+    {
+        // Назначаем кнопки только если это основной менеджер
+        if (Instance == this)
+        {
+            for (int i = 0; i < buttons.Length; i++) 
+            {
+                int index = i;
+                buttons[i].onClick.AddListener(() => ShowPanel(index));
+            }
+
+            // Инициализация: все панели скрыты
+            HideAllPanels();
+        }
+    }
+
+    private void Update() 
+    {
+        // Обрабатываем ввод только в основном менеджере
+        if (Instance == this)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab)) 
+            {
+                TogglePanels();
+            }
+            
+            // Добавляем обработку Escape
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                HideAllPanels();
+            }
+        }
+    }
+
+    public void TogglePanels()
+    {
+        if (!isOpened) 
+        {
+            panels[lastOpened].SetActive(true);
+            isOpened = true;
+        } 
+        else 
+        {
+            HideAllPanels();
+        }
+    }
+
+    public void ShowPanel(int panelIndex) 
+    {
+        HideAllPanels();
+        panels[panelIndex].SetActive(true);
+        lastOpened = panelIndex;
+        isOpened = true;
+    }
+
+    private void HideAllPanels()
+    {
+        foreach (var panel in panels) 
+        {
             panel.SetActive(false);
         }
-    }
-
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Tab)) {
-            if (!isOpened) {
-                // Показываем последнюю открытую панель
-                panels[lastOpened].SetActive(true);
-                isOpened = true;
-            } else {
-                // Скрываем все панели
-                foreach (var panel in panels) {
-                    panel.SetActive(false);
-                }
-                isOpened = false;
-            }
-        }
-    }
-
-    public void ShowPanel(int panelIndex) {
-        for (int i = 0; i < panels.Length; i++) {
-            panels[i].SetActive(i == panelIndex);
-            if (i == panelIndex) {
-                lastOpened = i;
-            }
-        }
-        isOpened = true; // Устанавливаем флаг, что панель открыта
+        isOpened = false;
     }
 }
