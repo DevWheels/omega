859fdc8e7818376d50f556279849fe6204bf371b
Lukhern <90791937+Luriko@users.noreply.github.com>
2025-06-14 09:25:54 +0200

    added new script, fixed buttons

--- Assets/Script/Items/ItemConfig.cs
+++ Assets/Script/Items/ItemConfig.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
-
-public class ItemConfig : ScriptableObject
-{
-
+public class ItemConfig : ScriptableObject {
     [Header("������� ��������������")]
     // public string Name = "";
     public string Description = "�������� ��������";
+
     public Sprite icon = null;
 
     public bool isHealing;
@@ -18,39 +16,32 @@ public class ItemConfig : ScriptableObject
     public bool isMana;
     public float ManaPower;
 
-    [Header("������� ��������������")]
-    public int time;
+    [Header("������� ��������������")] public int time;
 
     [Header("�������������� ��� ��������")]
     public int Coins;
 
     public string PlayerPrefsName;
-    
 
-    
-    [Header("Basic Settings")]
-    public string itemName;
+
+    [Header("Basic Settings")] public string itemName;
     public ItemType itemType;
     public ItemRank Rank;
     public GameObject Prefab;
-    
-    [Header("Skills")]
-    public List<SkillType> Skills;
-    
-    [Header("Stat Ranges By Rank")]
-    public RankStatRanges dRankStats;
+
+    [Header("Skills")] public List<SkillType> Skills;
+
+    [Header("Stat Ranges By Rank")] public RankStatRanges dRankStats;
     public RankStatRanges cRankStats;
     public RankStatRanges bRankStats;
     public RankStatRanges aRankStats;
     public RankStatRanges sRankStats;
-    
+
     // [Header("Visuals")]
     // public Color itemColor = Color.white;
 
-    public RankStatRanges GetRangesForRank(ItemRank rank)
-    {
-        return rank switch
-        {
+    public RankStatRanges GetRangesForRank(ItemRank rank) {
+        return rank switch {
             ItemRank.D => dRankStats,
             ItemRank.C => cRankStats,
             ItemRank.B => bRankStats,
@@ -61,8 +52,7 @@ public class ItemConfig : ScriptableObject
     }
 
     [System.Serializable]
-    public class RankStatRanges
-    {
+    public class RankStatRanges {
         public Vector2Int health;
         public Vector2Int attack;
         public Vector2Int armor;
@@ -70,9 +60,10 @@ public class ItemConfig : ScriptableObject
         public SpecialStatType[] possibleSpecialStats;
         public Vector2[] specialStatRanges;
     }
-    
+
     public ItemRarity rarity;
 }
+
 public enum ItemRarity {
     Common,
     Uncommon,
